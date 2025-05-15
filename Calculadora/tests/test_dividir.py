e792b54ffdb2afd2cf5c12bd5ecc5e1bf49176e6
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-05-15 14:01:06 -0300

    feat: add testes unitários e melhora testes escritos anteriormente

--- Calculadora/tests/test_dividir.py
+++ Calculadora/tests/test_dividir.py
@@ -1,11 +1,15 @@
 import pytest
 
-@pytest.mark.parametrize("num1, num2, esperado", [(0.0, 3.5,0.0), (24.0, 2.0, 12.0), (35.0, -7.0, -5.0), (-42.0, -6.0, 7.0)])
-def test_dividir(num1, num2, esperado):
-    assert dividir(num1, num2) == esperado
+@pytest.mark.parametrize("num1, num2, esperado", [(0.0, 3.5,0.0), (24.5, 5.0, 4.9), (36.4, -7.0, -5.2), (-43.2, -6.0, 7.2), (6.3, 1, 6.3)])
+def test_dividir_float(num1, num2, esperado):
+    assert dividir(num1, num2) == esperado, "Teste da função dividir com números decimais"
+
+@pytest.mark.parametrize("num1, num2, esperado", [(0, 3,0), (25, 5, 5), (35, -7, -5), (-42, -6, 7), (5,1,5)])
+def test_dividir_inteiros(num1, num2, esperado):
+    assert dividir(num1, num2) == esperado, "Teste da função dividir com números inteiros"
 
 def test_dividir_por_zero():
     with pytest.raises(ValueError) as e:
         dividir(10.0, 0.0)
     
-    assert str(e.value) == "Não é possível dividir por zero"
+    assert str(e.value) == "Não é possível dividir por zero", "Teste da função dividir com divisor igual a zero"
