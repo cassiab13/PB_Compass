642871514556dbde9917836127d8b4e33c952005
Cassia <125088421+cassiab13@users.noreply.github.com>
2025-05-16 15:29:09 -0300

    feat: add fatorial method

--- Calculadora/tests/unit_tests/test_fatorial.py
+++ Calculadora/tests/unit_tests/test_fatorial.py
@@ -1,9 +1,9 @@
 import pytest
 from calculadora.Calculadora import Calculadora
 
-@pytest.mark.parametrize("num, esperado", [(1,1), (5,120), (25, 1.551121004*10**25)])
+@pytest.mark.parametrize("num, esperado", [(1,1), (5,120), (25, 15511210043330985984000000)])
 def test_fatorial_inteiros(calc, num , esperado):
-    assert calculadora.fatorial(num) == esperado
+    assert calc.fatorial(num) == esperado
 
 def test_fatorial_negativo(calc):
     with pytest.raises(ValueError) as e:
@@ -16,4 +16,4 @@ def test_fatorial_decimal(calc):
         assert str(e.value) == "Náo é possível calcular fatorial de número decimal"
 
 def test_fatorial_zero(calc):
-    assert calculadora.fatorial(0) == 1
+    assert calc.fatorial(0) == 1
